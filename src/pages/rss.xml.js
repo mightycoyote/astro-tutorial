1472f50354828e461b5248c0ecea2de2580f1d48
Sarah Wilkes <aremnius@gmail.com>
2024-03-14 12:00:38 -0400

    through unit 5

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,13 @@
+// this is what an RSS feed setup looks like. You also have to install Astro's RSS package
+
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>en-us</language>`,
+  });
+}
